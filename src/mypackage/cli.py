7bbbcff5c9199a7a3a1940fe70c1e6d4c47a8d7c
Diego Quintana <daquintanav@gmail.com>
2022-10-07 09:58:41 +0200

    first commit

---
+++ src/mypackage/cli.py
@@ -0,0 +1,24 @@
+from typing import Optional
+import typer
+
+from mypackage.config import SettingsModeEnum, settings
+
+app = typer.Typer()
+
+
+@app.command()
+def print_settings():
+    color = typer.colors.BLUE
+    bold = False
+
+    for k, v in settings:
+        if isinstance(v, list):
+            typer.secho(f"{k}:", color=color, bold=bold)
+            for e in v:
+                typer.secho(f"\t{e}", color=color, bold=bold)
+        else:
+            typer.secho(f"{k}: {v}", color=color, bold=bold)
+
+
+if __name__ == "__main__":
+    app()
---
+++ src/mypackage/config.py
@@ -0,0 +1,29 @@
+import enum
+import os
+import secrets
+from typing import Optional
+
+from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, validator
+
+
+class SettingsModeEnum(str, enum.Enum):
+    DEV = "dev"
+    PROD = "prod"
+    TEST = "test"
+
+
+class Settings(BaseSettings):
+
+    PROJECT_NAME: str
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    @validator("SENTRY_DSN", pre=True)
+    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
+        if bool(v) is False:
+            return None
+        return v
+
+    HF_TOKEN: Optional[str] = None
+    WANDB_API_KEY: Optional[str] = None
+
+settings = Settings()
